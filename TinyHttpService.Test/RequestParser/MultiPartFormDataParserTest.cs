98cefab7b72613ed7d6877d6cc2c24d61a8ff4a5
zh6335901 <zh6335901@hotmail.com>
2014-06-04 23:33:23 +0800

    add MultiPartFormDataParser and RebufferableStreamReader unit test

---
+++ TinyHttpService.Test/RequestParser/MultiPartFormDataParserTest.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TinyHttpService.RequestParser;
+using System.IO;
+using System.Text;
+
+namespace TinyHttpService.Test.RequestParser
+{
+    [TestClass]
+    public class MultiPartFormDataParserTest
+    {
+        private static readonly string multiParamsAndFilesTestData = "--boundry\r\n" +
+              @"Content-Disposition: form-data; name=""text""" + 
+              "\r\n\r\ntextdata" + 
+              "\r\n--boundry\r\n" +
+              @"Content-Disposition: form-data; name=""file""; filename=""first.txt"";" +
+              "\r\nContent-Type: text/plain\r\n\r\n" +
+              "aaaaaaa" + 
+              "\r\n--boundry\r\n" + 
+              @"Content-Disposition: form-data; name=""newfile""; filename=""second.txt"";" +
+              "\r\nContent-Type: text/plain\r\n\r\n" +
+              "bbbbbbb" +
+              "\r\n--boundry\r\n" +
+              @"Content-Disposition: form-data; name=""username""" +
+              "\r\n\r\nzhang" +
+              "\r\n--boundry--";
+
+        private static readonly string multiParamsTestData = "--boundry\r\n" +
+              @"Content-Disposition: form-data; name=""text""" +
+              "\r\n\r\ntextdata" +
+              "\r\n--boundry\r\n" +
+              @"Content-Disposition: form-data; name=""username""" +
+              "\r\n\r\nzhang" +
+              "\r\n--boundry--";
+
+        private static readonly string multiFilesTestData = 
+              "--boundry\r\n" +
+              @"Content-Disposition: form-data; name=""file""; filename=""first.txt"";" +
+              "\r\nContent-Type: text/plain\r\n\r\n" +
+              "aaaaaaa" +
+              "\r\n--boundry\r\n" +
+              @"Content-Disposition: form-data; name=""newfile""; filename=""second.txt"";" +
+              "\r\nContent-Type: text/plain\r\n\r\n" +
+              "bbbbbbb" +
+              "\r\n--boundry--";
+
+        [TestMethod]
+        public void CanParseMultiParamsData() 
+        {
+            using (MemoryStream ms = new MemoryStream(Encoding.Default.GetBytes(multiParamsTestData)))
+            {
+                MultiPartFormDataParser parser = new MultiPartFormDataParser(ms);
+                Assert.AreEqual(parser.Parameters["text"], "textdata");
+                Assert.AreEqual(parser.Parameters["username"], "zhang");
+            }
+        }
+
+        [TestMethod]
+        public void CanParseMultiFilesData() 
+        {
+            using (MemoryStream ms = new MemoryStream(Encoding.Default.GetBytes(multiFilesTestData)))
+            {
+                MultiPartFormDataParser parser = new MultiPartFormDataParser(ms);
+                Assert.AreEqual(parser.Files.Count, 2);
+                Assert.AreEqual(parser.Files[0].Filename, "first.txt");
+                Assert.AreEqual(parser.Files[0].Name, "file");
+                Assert.AreEqual(parser.Files[1].Filename, "second.txt");
+                Assert.AreEqual(parser.Files[1].Name, "newfile");
+
+                using (var sw1 = new StreamReader(parser.Files[0].Data))
+                using (var sw2 = new StreamReader(parser.Files[1].Data))
+                {
+                    string file1Str = sw1.ReadToEnd();
+                    string file2Str = sw2.ReadToEnd();
+
+                    Assert.AreEqual(file1Str, "aaaaaaa");
+                    Assert.AreEqual(file2Str, "bbbbbbb");
+                }
+            }
+        }
+
+        [TestMethod]
+        public void CanParseMultiParamsAndFilesData()
+        {
+            using (MemoryStream ms = new MemoryStream(Encoding.Default.GetBytes(multiParamsAndFilesTestData))) 
+            {
+                MultiPartFormDataParser parser = new MultiPartFormDataParser(ms);
+                Assert.AreEqual(parser.Parameters["text"], "textdata");
+                Assert.AreEqual(parser.Parameters["username"], "zhang");
+
+                Assert.AreEqual(parser.Files.Count, 2);
+                Assert.AreEqual(parser.Files[0].Filename, "first.txt");
+                Assert.AreEqual(parser.Files[0].Name, "file");
+                Assert.AreEqual(parser.Files[1].Filename, "second.txt");
+                Assert.AreEqual(parser.Files[1].Name, "newfile");
+
+                using (var sw1 = new StreamReader(parser.Files[0].Data)) 
+                using (var sw2 = new StreamReader(parser.Files[1].Data))
+                {
+                    string file1Str = sw1.ReadToEnd();
+                    string file2Str = sw2.ReadToEnd();
+
+                    Assert.AreEqual(file1Str, "aaaaaaa");
+                    Assert.AreEqual(file2Str, "bbbbbbb");
+                }
+            }
+        }
+    }
+}
---
+++ TinyHttpService.Test/Utils/RebufferableStreamReaderTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Text;
+using TinyHttpService.Utils;
+
+namespace TinyHttpService.Test.Utils
+{
+    [TestClass]
+    public class RebufferableStreamReaderTest
+    {
+        private static readonly string readerTestString = "ni hao wo shi zhanghao\r\nthis is test!";
+        private static readonly string lineBufferTestString = "hahaha\r\nhehe";
+        private static readonly string noLineBufferTestString = "this buffer no line";
+
+        [TestMethod]
+        public void CanReadLineWhenReaderNoBuffer()
+        {
+            using (var ms = new MemoryStream(Encoding.Default.GetBytes(readerTestString))) 
+            {
+                RebufferableStreamReader reader = new RebufferableStreamReader(ms);
+                string line = reader.ReadLine();
+                string nextLine = reader.ReadLine();
+
+                Assert.AreEqual(line, "ni hao wo shi zhanghao");
+                Assert.AreEqual(nextLine, "this is test!");
+            }
+        }
+
+        [TestMethod]
+        public void CanReadLineWhenLineInBuffer() 
+        {
+            using (var ms = new MemoryStream(Encoding.Default.GetBytes(readerTestString)))
+            {
+                RebufferableStreamReader reader = new RebufferableStreamReader(ms);
+                reader.Rebuffer(Encoding.Default.GetBytes(lineBufferTestString));
+
+                var line = reader.ReadLine();
+                var secondLine = reader.ReadLine();
+                var thirdLine = reader.ReadLine();
+
+                Assert.AreEqual(line, "hahaha");
+                Assert.AreEqual(secondLine, "hehe" + "ni hao wo shi zhanghao");
+                Assert.AreEqual(thirdLine, "this is test!");
+
+                reader.Rebuffer(Encoding.Default.GetBytes("hello world"));
+                var fourLine = reader.ReadLine();
+                Assert.AreEqual(fourLine, "hello world");
+            }
+        }
+
+        public void CanReadLineWhenReaderHaveBufferAndNoLineInBuffer() 
+        {
+            using (var ms = new MemoryStream(Encoding.Default.GetBytes(readerTestString)))
+            {
+                RebufferableStreamReader reader = new RebufferableStreamReader(ms);
+                reader.Rebuffer(Encoding.Default.GetBytes(noLineBufferTestString));
+
+                var line = reader.ReadLine();
+                var secondLine = reader.ReadLine();
+                var thirdLine = reader.ReadLine();
+
+                Assert.AreEqual(line, noLineBufferTestString + "ni hao wo shi zhanghao");
+                Assert.AreEqual(secondLine, "this is test!");
+                Assert.AreEqual(thirdLine, string.Empty);
+            }
+        }
+    }
+}
